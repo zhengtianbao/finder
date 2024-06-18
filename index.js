6b246ad78d00a24f724b2ba0bf286768bc79de6c
zhengtianbao <china.zhengtianbao@gmail.com>
2024-06-18 15:12:44 +0800

    add close window behave

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron'
 
 let mainWindow;
+
 app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -9,4 +10,22 @@ app.whenReady().then(() => {
     },
   });
   mainWindow.loadFile('dist/index.html');
+});
+
+// for mac os
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// for windows and linux
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
